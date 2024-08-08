e00fd45b3f637bc35bb7ee33f92418fd1bdf4638
Karan Meghe <karanmeghe87@gmail.com>
2024-08-08 09:28:55 +0530

    add: tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,30 @@ import authService from "./appWrite/auth";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  // const dispatch = useDispatch();
-  return (
-    <>
-      <Header />
-      <Footer />
-    </>
-  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between items-center bg-[#EEEDEB]">
+      <div className="w-full  flex flex-col items-center">
+        <Header />
+        <main>TODO:{/* <Outlet></Outlet> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
